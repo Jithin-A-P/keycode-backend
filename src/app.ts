ba868701a61d66115cc8ba2e8f1828836f87ab61
Vigneshwar <vigneshwar@keyvalue.systems>
2023-09-15 14:01:29 +0530

    add websocket game

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,9 @@ import errorMiddleware from './middleware/error.middleware'
 import responseMiddleware from './middleware/response.middleware'
 import logger from './utils/winston.logger'
 import { Server as SocketIOServer } from 'socket.io'
-import http from 'http'
+import http from 'http';
+import { createClient } from "redis";
+import { createAdapter } from "@socket.io/redis-adapter";
 
 const server = express()
 const PORT = process.env.PORT
@@ -22,6 +24,10 @@ server.use(loggerMiddleware)
 server.use('/api/users', userRoute)
 server.use(responseMiddleware)
 server.use(errorMiddleware)
+
+const pubClient = createClient({  url: "redis://localhost:6379" });
+const subClient = pubClient.duplicate();
+
 
 dataSource
   .initialize()
@@ -37,45 +43,54 @@ dataSource
     })
   })
 
-const websocketServer = http.createServer()
-websocketServer.listen(5050, () => {
-  logger.log({
-    level: 'info',
-    message: 'WebSocket Server listening on port 5050',
-  })
-})
-
-const socketioWebSocket = new SocketIOServer(websocketServer)
-
+const websocketServer = http.createServer();
+const socketioWebSocket = new SocketIOServer(websocketServer, {cors: {origin: "*"}});
+const screenPool = {};
 socketioWebSocket.on('connection', (socket) => {
-  logger.log({ level: 'info', message: 'Socket.io HTTP client connected' })
+  logger.log({ level: 'info', message: 'Socket.io HTTP client connected' });
+  const connectionDetails = socket.handshake.query;
+  const screenId: string = connectionDetails["screenId"] as string;
+  console.log(connectionDetails);
+  if(connectionDetails["type"] === 'playerA'){
+    // check if any game is in queue already, if yes reject
+    console.log("joining to room", connectionDetails["gameRoomId"])
+     socket.join(connectionDetails["gameRoomId"]);
+  }
+    if(connectionDetails["type"] === 'screen'){
+      // check if any game is in queue already, if yes reject
+      console.log("joining to room", connectionDetails["gameRoomId"])
+      
+      screenPool[screenId] = socket;
+  }
+  socket.on("game_started", (gameRoomId: string)=> {
+    console.log("game starting");
+    socketioWebSocket.emit("show_buttons");
+  })
+  socket.on("button_click", (data)=> {
+    console.log("sending button click data");
+    screenPool[screenId].emit("button_click_data", data); 
+  })
 
   socket.on('message', (message) => {
     logger.log({
       level: 'info',
       message: `Received Socket.io HTTP message: ${message}`,
     })
-  })
+  });
+
 
   socket.on('disconnect', () => {
     logger.log({ level: 'info', message: 'Socket.io HTTP client disconnected' })
-  })
-})
+  });
+});
 
-socketioWebSocket.on('connection', (socket) => {
-  logger.log({ level: 'info', message: 'Socket.io WebSocket client connected' })
-
-  socket.on('message', (message) => {
+Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+  socketioWebSocket.adapter(createAdapter(pubClient, subClient));
+  websocketServer.listen(5050, () => {
     logger.log({
       level: 'info',
-      message: `Received Socket.io WebSocket message: ${message}`,
+      message: 'WebSocket Server listening on port 5050',
     })
   })
-
-  socket.on('disconnect', () => {
-    logger.log({
-      level: 'info',
-      message: 'Socket.io WebSocket client disconnected',
-    })
-  })
-})
+  socketioWebSocket.emit("hello", "world");
+});
